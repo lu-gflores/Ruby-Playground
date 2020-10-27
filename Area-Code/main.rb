5a9a5dd6c264223776d74a7b90a62974260c0da4
George Flores <georgeflores886@gmail.com>
2020-10-27 12:49:44 -0400

    solved area code exercise

---
+++ Area-Code/main.rb
@@ -0,0 +1,33 @@
+dial_book = {
+    "newyork" => "212",
+    "newbrunswick" => "732",
+    "edision" => "908",
+    "plainsboro" => "609",
+    "sanfrancisco" => "301",
+    "miami" => "305",
+    "paloalto" => "650",
+    "evanston" => "847",
+    "orlando" => "407",
+    "lancaster" => "717"
+}
+
+def get_city_names(somehash)
+    cities =  somehash.keys
+    return cities
+end
+
+def get_area_code(somehash, key)
+   return somehash[key]
+end
+
+loop do
+    puts "Do you want to lookup an area code based on a city name(Y/N)?"
+    answer = gets.chomp.downcase
+    break if answer != "y"
+    puts "What city would you like to get the area code for?"
+    puts get_city_names(dial_book)
+    puts "Enter your selection: "
+    city = gets.chomp.downcase
+    area_code = get_area_code(dial_book, city)
+    puts "the area code for #{city} is #{area_code}"
+end
